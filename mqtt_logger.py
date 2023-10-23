dea657db74abe91e3648e1735769162fee28bd59
ChumakovJA <chumakov_ja@mail.ru>
2023-10-23 16:39:25 +0300

    4

--- mqtt_logger.py
+++ mqtt_logger.py
@@ -1,13 +1,12 @@
 import configparser
 
 import paho.mqtt.client as mqtt
-import sqlite3
-
+import pymysql
 
 import pathlib
 
 TOPIC = 'ESP_Easy/#'
-
+#connection = None
 
 def on_connect(mqtt_client, user_data, flags, conn_result):
     mqtt_client.subscribe(TOPIC)
@@ -15,14 +14,12 @@ def on_connect(mqtt_client, user_data, flags, conn_result):
 
 def on_message(mqtt_client, user_data, message):
     payload = message.payload.decode('utf-8')
-    if(message.topic != 'ESP_Easy/status' ):
+    if message.topic.startswith('ESP_Easy/status') != True :
         db_conn = user_data['db_conn']
-        sql = 'INSERT INTO sensors_data (topic, value) VALUES (?, ?)'
-        cursor = db_conn.cursor()
-        cursor.execute(sql, (message.topic, payload))
-        db_conn.commit()
-        cursor.close()
-
+        with db_conn.cursor() as cursor:
+           insert_query = "INSERT INTO status_topic (topic, value) VALUES ('" + message.topic + "', '" + payload + "');"
+           cursor.execute(insert_query)
+           db_conn.commit()
 
 def main():
 
@@ -30,23 +27,23 @@ def main():
     config = configparser.ConfigParser()
     config.read(basedir / 'config.ini')
 
+    try:
+        connection = pymysql.connect(
+            host=config.get('mysql_server','host'),
+            port=config.getint('mysql_server','port'),
+            user=config.get('mysql_server','user'),
+            password=config.get('mysql_server','password'),
+            database=config.get('mysql_server','database'),
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        print("successfully connected...")
 
-    db_conn = sqlite3.connect(config.get('mqqt_server','DATABASE_FILE'))
-    sql = """
-    CREATE TABLE IF NOT EXISTS sensors_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        topic TEXT NOT NULL,
-        value DECIMAL(10,2) NOT NULL,
-        ts DATE DEFAULT (datetime('now','localtime'))
-    )
-    """
-    cursor = db_conn.cursor()
-    cursor.execute(sql)
-    cursor.close()
+    except Exception as ex:
+        print("Connection refused...")
 
     mqtt_client = mqtt.Client(config.get('mqqt_server','MQTT_BROKER_URL').replace('.','_'))
     mqtt_client.username_pw_set(config.get('mqqt_server','MQTT_USERNAME'), config.get('mqqt_server','MQTT_PASSWORD'))
-    mqtt_client.user_data_set({'db_conn': db_conn})
+    mqtt_client.user_data_set({'db_conn': connection})
 
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
